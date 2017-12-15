24c065fca9ae9b98ec7bbe2496814d931c363802
Oskar Kobierecki <oskar.kobierecki@o2.pl>
2017-12-15 01:44:45 +0100

    Finished header

---
+++ inc/header.php
@@ -0,0 +1,54 @@
+<?php
+    @session_start(); //inicjalizuje sesje
+    if(isset($_SESSION['user'])){ //jezeli jest to pierwsza sesja to
+        $session = $_SESSION['user']; //przypisuje zmienna
+    } else {
+        $session = null;
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Photogallery</title>
+        <link rel="stylesheet" type="text/css" href="css/vendor/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/vendor/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <div class="row">
+                    <a href="#" class="logo pull-left">Photogallery</a>
+                    <nav class="pull-right">
+                        <button type="button" class="btn" data-toggle="collapse" data-target="#menu">
+                            <i class="fa fa-navicon"></i>
+                        </button>
+                        <div id="menu" class="collapse">
+                            <ul>
+                                <li><a href="">Home</a></li>
+                                <li><a href="">Gallery</a></li>
+                                <li><a href="">About</a></li>
+                                <li><a href="">Contact</a></li>
+                                <?php
+                                    if($session == null) {
+                                    
+                                ?>
+                                    <li class="login"><a href="">Login</a></li>
+                                    <li class="register"><a href="">Register</a></li>
+                                <?php
+                                    } else {
+
+                                ?>
+                                    <li class="logout"><a href="">Logout</a></li>
+                                <?php
+                                    }
+                                ?>  
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </header>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    include 'inc/header.php';
+    include 'inc/slider.php';
+    include 'inc/gallery-home.php';
+    include 'inc/recent-uploads.php';
+    include 'inc/top-uploaders.php';
+    include 'inc/footer.php';
+?>      <script src="js/vendor/bootstrap.min.js"></script>
+        <!-- SCRIPTS -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script>
+            window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>');
+        </script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script> 
+            $.fn.modal || document.write('<script src="/js/vendor/bootstrap.min.js">\x3C/script>')
+        </script>
+
+    </body>
+</html>
